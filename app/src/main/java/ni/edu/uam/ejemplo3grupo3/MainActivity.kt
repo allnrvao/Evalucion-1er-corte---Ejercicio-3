9cc49da16ecec78eff0beab7a45abae094e2c19c
Guillermo Vega <168846949+Guillermo-UAM27@users.noreply.github.com>
2026-04-15 17:36:37 -0600

    Archivo

---
+++ app/src/main/java/ni/edu/uam/ejemplo3grupo3/MainActivity.kt
@@ -0,0 +1,47 @@
+package ni.edu.uam.ejemplo3grupo3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ni.edu.uam.ejemplo3grupo3.ui.theme.Ejemplo3Grupo3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ejemplo3Grupo3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ejemplo3Grupo3Theme {
+        Greeting("Android")
+    }
+}
