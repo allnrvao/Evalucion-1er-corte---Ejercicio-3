fe96c62376d36247dbd77f95cdef6ada7f3d0d85
AJCD <ajcortes@uamv.edu.ni>
2026-04-15 18:00:44 -0600

    Tercera parte

--- app/src/main/java/ni/edu/uam/ejemplo3grupo3/MainActivity.kt
+++ app/src/main/java/ni/edu/uam/ejemplo3grupo3/MainActivity.kt
@@ -6,21 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -101,6 +115,29 @@ class MainActivity : ComponentActivity() {
                     Text("Registrar Pedido")
                 }
             }
+        }
+    }
+}
+@Composable
+fun RegistroExitosoBanner() {
+    Surface(
+        color = MaterialTheme.colorScheme.secondaryContainer,
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(modifier = Modifier.padding(12.dp)) {
+            Icon(Icons.Default.CheckCircle, contentDescription = null)
+            Text("¡Pedido registrado con éxito!")
+        }
+    }
+}
+
+@Composable
+fun PedidoCard(info: String) {
+    ElevatedCard(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("Resumen:", style = MaterialTheme.typography.labelLarge)
+            Text(text = info, style = MaterialTheme.typography.headlineSmall)
         }
     }
 }
